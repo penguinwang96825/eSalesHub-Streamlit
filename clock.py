26499b6e1d6c41acfb0b0bfa1affb36265ed2d3f
penguinwang96825 <penguinwang@smail.nchu.edu.tw>
2022-03-23 00:25:28 +0800

    [feat] Update status code

--- clock.py
+++ clock.py
@@ -10,7 +10,7 @@ def timed_job_awake_heroku_app():
     url = 'https://esaleshub-demo.herokuapp.com/'
     res = requests.get(url)
     print("Wake up every 15 minutes!")
-    print(f'Content: \n{res.content}')
+    print(f'Status Code: {res.status_code}')
 
 
 scheduler.start()
